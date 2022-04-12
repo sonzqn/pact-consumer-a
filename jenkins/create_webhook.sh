af05f5420a213fdbe61052561a3aadb87d4b67b2
sonzqn <sonzqn@gmail.com>
2022-04-12 15:35:45 +0700

    update

--- jenkins/create_webhook.sh
+++ jenkins/create_webhook.sh
@@ -1,6 +1,68 @@
-curl 'http://localhost:9292/webhooks/provider/provider-x/consumer/consumer-a' \
-  -H 'Accept: application/hal+json, application/json, */*; q=0.01' \
-  -H 'Content-Type: application/json' \
-  -H 'X-Interface: HAL Browser' \
-  --data-raw $'{\n  "consumer": {\n    "name": "consumer-a"\n  },\n  "provider": {\n    "name": "provider-x"\n  },\n  "request": {\n    "method": "POST",\n    "url": "http://127.0.0.1:8080/job/provider-x-run-contract-tests/buildWithParameters?pactConsumerTags=${pactbroker.consumerVersionTags}&pactConsumerName=${pactbroker.consumerName}",\n    "headers": {\n      "Accept": "application/json",\n      "Authorization": "Basic c29ubmQ0OjExNWJhNzcyNjg0OGU5ZTJmZWYzZGU5MzdmODJkZDU2Mzg="\n    }\n  },\n  "events": [\n    {\n      "name": "contract_content_changed"\n    }\n  ]\n}\n' \
-  --compressed
+#!/bin/bash
+jenkins_url="http://127.0.0.1:8080"
+pact_broker_url="http://localhost:9292"
+authorization="c29ubmQ0OjExNWJhNzcyNjg0OGU5ZTJmZWYzZGU5MzdmODJkZDU2Mzg="
+consumer_name="consumer-b"
+curl "${pact_broker_url}/groups/${consumer_name}.csv" > consumer.csv
+providers=
+while IFS=, read -r id name field3 field4 field5 index provider_ids
+do
+    if [ "$name" = "$consumer_name" ]
+    then
+        providers="$provider_ids"
+    fi
+done < consumer.csv
+echo "${providers}"
+while IFS=, read -r id name field3
+do
+    providers=$(echo "${providers}" | sed "s/$id/$name/")
+done < consumer.csv
+echo "${providers}"
+
+provider_names=($(echo "$providers" | tr ',' "\n"))
+
+generate_post_data()
+{
+  consumerVersionTags='${pactbroker.consumerVersionTags}'
+  consumerName='${pactbroker.consumerVersionTags}'
+  cat <<EOF
+	{
+    "consumer": {
+      "name": "${consumer_name}"
+    },
+    "provider": {
+      "name": "${provider_name}"
+    },
+    "request": {
+      "method": "POST",
+      "url": "${jenkins_url}/job/${provider_name}-run-contract-tests/buildWithParameters?pactConsumerTags=${consumerVersionTags}&pactConsumerName=${consumerName}",
+      "headers": {
+        "Accept": "application/json",
+        "Authorization": "Basic $authorization"
+      }
+    },
+    "events": [
+      {
+        "name": "contract_content_changed"
+      }
+    ]
+  }
+EOF
+}
+
+for provider_name in "${provider_names[@]}"
+do
+  webhook_status=$(curl "${pact_broker_url}/pacts/provider/${provider_name}/consumer/${consumer_name}/webhooks")
+  #echo "${webhook_status}"
+  if [[ "$webhook_status" == *"A webhook for the pact between ${consumer_name} and ${provider_name}"* ]]; then
+    echo "Webhook already exists!"
+  else
+    body=$(generate_post_data)
+        curl "${pact_broker_url}/webhooks/provider/${provider_name}/consumer/${consumer_name}" \
+    	  -H 'Accept: application/hal+json, application/json, */*; q=0.01' \
+    	  -H 'Content-Type: application/json' \
+    	  -H 'X-Interface: HAL Browser' \
+    	  --data-raw $"${body}" \
+      --compressed
+  fi
+done
