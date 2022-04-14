6d42eca1bddbd40e2af72304ec08f4d246ff8e25
sonzqn <sonzqn@gmail.com>
2022-04-14 14:06:43 +0700

    update

--- src/main/java/io/pact/workshop/product_catalogue/models/Product.java
+++ src/main/java/io/pact/workshop/product_catalogue/models/Product.java
@@ -7,7 +7,6 @@ public class Product {
   private final Long id;
   private final String name;
   private final String type;
-  private final String tag;
   private final String description;
   private final Long price;
 }
